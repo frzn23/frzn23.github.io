46d4e067107ee35fca1b070fb396f70c3f85590f
frzn23 <78311311+frzn23@users.noreply.github.com>
2021-06-27 20:11:49 +0530

    Add files via upload

---
+++ laundry/js/app.js
@@ -0,0 +1,12 @@
+
+$('document').ready(function(){
+
+var type = new Typed('#typed',{
+    stringsElement: '#typed-strings',
+    strings: ['In The Best Quality','Within 12 Hours','In The Best Price'],
+    backSpeed:40,
+    typeSpeed:40,
+    loop:true,
+    backDelay:1000
+})
+});
